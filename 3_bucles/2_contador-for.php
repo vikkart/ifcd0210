609af2a0fdbdc641058b3f57a40c49b925e28a3e
vikkart <victorpi444@gmail.com>
2025-02-26 18:44:36 +0100

    22

--- 3_bucles/2_contador-for.php
+++ 3_bucles/2_contador-for.php
@@ -1,7 +1,9 @@
 <link rel="stylesheet" type="text/css" href="/ifcd0210/estilo.css">
 <?php 
 $variable;
-for ($variable = 0; $variable <= 10; $variable++) { 
-    echo ("<ul><li>" . ($variable) . "</li></ul>");
+echo "<ul>";
+for ($variable = 1; $variable <= 10; $variable++) { 
+    echo ("<li>$variable</li>");
 }
-?>
+echo "</ul>"
+?> 
--- 3_bucles/3_contador-while.php
+++ 3_bucles/3_contador-while.php
@@ -1,7 +1,9 @@
 <link rel="stylesheet" type="text/css" href="/ifcd0210/estilo.css">
 <?php 
 $variable = 1;
+echo ("<ul>");
 while ($variable <= 10) {
-    echo ("<ul><li>" . ($variable) . "</li></ul>");
+    echo ("<li>$variable</li>");
     $variable++;
 }
+echo ("</ul>")
