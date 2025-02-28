998b8636019b7df76ff2ef9379d74a0375a846f9
vikkart <victorpi444@gmail.com>
2025-02-28 18:31:51 +0100

    array tocho

--- 4_arrays/6_temperaturas-matriz.php
+++ 4_arrays/6_temperaturas-matriz.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Temperaturas matriz</title>
+    <link rel="stylesheet" type="text/css" href="/ifcd0210/estilo.css">
+
+</head>
+<body>
 <?php
 
 /* 
@@ -22,3 +32,29 @@
     - Iterarlo y mostrar el texto "el lunes por la mañana hay 17ºC, y por la tarde 8ºC, el martes por la mañana..."
 
 */
+$dia = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
+
+$horario = ["mañana", "tarde" ];
+
+$temperaturas = []; 
+
+
+for ($i=0; $i < count($dia); $i++) { 
+    $temperaturas[$dia[$i]]= [
+    $horario[0]=> rand(0,30),
+    $horario[1]=> rand(0,30)
+];
+};
+for ($i=0; $i < count($dia); $i++) { 
+    echo "El " . $dia[$i] . " por la mañana hay " . $temperaturas[$dia[$i]][$horario[0]] . "ºC, y por la tarde " . $temperaturas[$dia[$i]][$horario[1]] . "ºC.<br>";
+};
+
+/* echo "El " . $dia[$i] . " por la mañana hay " . $temperaturas[$dia[$i]][$horario[0]] . "ºC, y por la tarde " . $temperaturas[$dia[$i]][$horario[1]] . "ºC.<br>";
+ */
+//echo $temperaturas[$dia[$i]][$horario[0]] . "<br>";
+
+//Se pone temperaturas, con el array dias dentro para asignar sus valores como claves, después el $i para que el bucle vaya progresando y vaya contando los valores, es decir, los días. Después, se le asigna las dos "cordenadas" que es mañana y tarde. 
+//para imprimir también hay que especificiar. 
+?>
+</body>
+</html>
